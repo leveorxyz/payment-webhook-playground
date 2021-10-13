45825e8537014d3c6e5396c6362f2f1aa8e0b04a
Sihan <sihantawsik@gmail.com>
2021-10-13 18:37:08 +0600

    Update paypal client-id

--- src/components/PaypalPayment.js
+++ src/components/PaypalPayment.js
@@ -6,7 +6,7 @@ import {
 
 export default function PaypalPayment() {
   return (
-    <PayPalScriptProvider options={{ 'client-id': 'test' }}>
+    <PayPalScriptProvider options={{ 'client-id': 'AVXS2Vs7A7-pEKTmamJt42cYtl_sZY5DEW86JUB-Go2kTJAuWGMzSHV9j7bVu1JsSucNFmWBkjvYPqoo' }}>
       <PayPalButtons
         fundingSource={FUNDING.PAYPAL}
         style={{ layout: 'horizontal' }}
